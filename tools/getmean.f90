ecc2490a46914f8c0ce2a46a37492788bdc576d3
wangliang <wangliang0222@gmail.com>
2018-05-23 10:50:36 +0800

    add getmean

---
+++ tools/getmean.f90
@@ -0,0 +1,23 @@
+program crossnorm
+use sacio
+use module_crossnorm
+implicit none
+character(len=1024) :: arg
+character(len=:) ,allocatable :: file
+integer :: i, flag
+real,allocatable,dimension(:) :: x
+type(sachead) :: head
+real :: sum, mean
+
+
+call get_command_argument(1, arg)
+file = arg
+call sacio_readsac(file, head, x, flag)
+sum = 0
+do i=1, size(x)
+    sum = sum + x(i)
+end do
+mean = sum / size(x)
+write(*,*) mean
+
+end program crossnorm
